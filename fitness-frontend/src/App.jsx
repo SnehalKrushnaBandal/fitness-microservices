21d1827fcda4a19e7871f66944c2557b4aa4b13c
SnehalBandal <skbandal02@gmail.com>
2026-07-11 15:08:58 +0530

    Added React frontend with Vite and Redux setup

---
+++ fitness-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+// import './App.css'
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router";
+import Button from "@mui/material/Button";
+
+function App() {
+
+  return (
+    <>
+    <Router>
+      <Button variant="contained">
+        LOGIN
+      </Button>
+    </Router>
+
+
+
+     
+     
+    </>
+  )
+}
+
+export default App
